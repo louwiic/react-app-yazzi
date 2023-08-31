f4daa22e53857cc38b747460fa988c0f5ff89be2
loic batonnet <louwiic@gmail.com>
2023-08-31 22:52:21 +0400

    homepage : wip: added button and styles homepage

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,36 +1,80 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { Col, Container, Row } from 'reactstrap'
+import { Button, Col, Row } from 'reactstrap'
+import { images } from 'theme'
 import styles from './dashboard.module.scss'
-
-const BackgroundContainer = ({ children }) => {
-  return (
-    <Container fluid className={styles.bgcontainer}>
-      {children}
-    </Container>
-  )
-}
 
 const Dashboard = () => {
   /* const dispatch = useDispatch() */
   /*  const { me } = useSelector((state) => state.app) */
+
   return (
-    <div className="App">
-      <BackgroundContainer>
-        {/* */}
-        <Row>
-          <Col xs={12} md={3} className="bg-primary">
-            <h2>Colonne 1</h2>
-          </Col>
-          <Col xs={12} md={6} className={styles.middle}>
-            <h1 className={styles.titlebloc}>
-              Site de faire-part digital entièrement personnalisé
-            </h1>
-          </Col>
-          <Col xs={12} md={3} className="bg-danger">
-            <h2>Colonne 3</h2>
-          </Col>
-        </Row>
-      </BackgroundContainer>
+    <div fluid className={`${styles.bgcontainer} App`}>
+      <Row style={{ height: '100vh', width: '100%' }}>
+        <Col md={2} style={{}}>
+          <div className={styles.logocontainer}>
+            <img src={images.logo} alt="img_heart" />
+          </div>
+        </Col>
+        <Col md={8}>
+          <div className={styles.middle} style={{ marginTop: 60 }}>
+            <div className={styles.containertitle}>
+              <div className={styles.imgheart}>
+                <img src={images.hearts1} alt="img_heart" />
+              </div>
+              <h1 className={styles.titlebloc}>
+                Site de faire-part digital entièrement personnalisé
+              </h1>
+            </div>
+            <div style={{ marginTop: 20 }}>
+              <span className={styles.subtitlebloc}>
+                Plus simple | Écologique | Unique
+              </span>
+              <span className={styles.subtitleblocbis}>
+                Gérer votre liste d’invités en toute tranquillité
+              </span>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignSelf: 'center',
+                marginTop: '80px',
+              }}
+            >
+              <button
+                onClick={() => console.log(' *** click ***')}
+                className={styles.mybutton}
+              >
+                {' '}
+                Et si on se disait oui ?
+              </button>
+              <div
+                style={{
+                  height: 45,
+                  width: 90,
+                  marginTop: 30,
+                  marginLeft: 20,
+                  backgroundColor: 'orangered',
+                }}
+              >
+                <img src={images.arrowleft} alt="img_heart" />
+              </div>
+              {/*  <span>En moins de 5 minutes</span> */}
+            </div>
+          </div>
+        </Col>
+        <Col
+          md={2}
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <div className={styles.asiderigthcontainer}>
+            <img src={images.flowerR} alt="flowerR" />
+          </div>
+        </Col>
+      </Row>
     </div>
   )
 }
