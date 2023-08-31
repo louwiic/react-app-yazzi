0f3830f58a73bb8959830391fa0f6bcc03d8ba25
Loic Batonnet <loic.batonnet@pleaseapp.com>
2023-08-31 16:48:52 +0400

    add style center element home

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { Col, Container, Row } from 'reactstrap'
 import styles from './dashboard.module.scss'
 
 const BackgroundContainer = ({ children }) => {
-  return <div className={styles.bgcontainer}>{children}</div>
+  return (
+    <Container fluid className={styles.bgcontainer}>
+      {children}
+    </Container>
+  )
 }
 
 const Dashboard = () => {
@@ -11,9 +16,20 @@ const Dashboard = () => {
   return (
     <div className="App">
       <BackgroundContainer>
-        <h1 className={styles.titlebloc}>
-          Site de faire-part digital entièrement personnalisé
-        </h1>
+        {/* */}
+        <Row>
+          <Col xs={12} md={3} className="bg-primary">
+            <h2>Colonne 1</h2>
+          </Col>
+          <Col xs={12} md={6} className={styles.middle}>
+            <h1 className={styles.titlebloc}>
+              Site de faire-part digital entièrement personnalisé
+            </h1>
+          </Col>
+          <Col xs={12} md={3} className="bg-danger">
+            <h2>Colonne 3</h2>
+          </Col>
+        </Row>
       </BackgroundContainer>
     </div>
   )
